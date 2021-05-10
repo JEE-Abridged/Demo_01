66e5b06d8e8dbf1488de33b29661dec5789ec47f
Kishor Kumar <kishorgn@gmail.com>
2021-05-10 10:58:47 +0530

    After sharing to Chaitanya

--- SimpleProject01/src/com/cg/app/Demo02.java
+++ SimpleProject01/src/com/cg/app/Demo02.java
@@ -4,6 +4,7 @@ public class Demo02 {
 
 	public static void main(String[] args) {
 		System.out.println("This is second program.");
+		System.out.println("Line added after sharing to Chaitanya");
 
 	}
 
