688bea33a103e9028173349427276673d9eb1eb1
Kishor Kumar <kishorgn@gmail.com>
2021-05-10 10:31:51 +0530

    Demo01 - Even / Odd

--- SimpleProject01/src/com/cg/app/Demo01.java
+++ SimpleProject01/src/com/cg/app/Demo01.java
@@ -4,7 +4,15 @@ public class Demo01 {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome");
-
+		for(int c = 0 ; c < 10 ; c++) {
+			if(c % 2 == 0) {
+				System.out.println("Even number");
+			}
+			else {
+				System.out.println("Odd Number");
+			}
+		}
+		System.out.println("End of the program.");
 	}
 
 }
